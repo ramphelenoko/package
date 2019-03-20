f17d114d69325499e210a984313056a6da7db7b4
ramphelenoko <48709253+ramphelenoko@users.noreply.github.com>
2019-03-20 15:02:43 +0200

    Update recursion.py

--- package_name/recursion.py
+++ package_name/recursion.py
@@ -10,7 +10,8 @@ def fibonacci(n):
     '''Return nth term in fibonacci sequence'''
     if n <= 1:
        return 1
-    return fibonacci(n - 2) + fibonacci(n - 1)
+    else:
+        return fibonacci(n - 2) + fibonacci(n - 1)
 
 def factorial(n):
 
